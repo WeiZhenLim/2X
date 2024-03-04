e362aebb4e37038e4c5e46eb1a13a66d13043da7
Wei Zhen Lim <WeiZhen.Lim@2x.marketing>
2024-03-04 11:27:20 +0800

    compile_csv to cater for one file & skip row

--- python_project_2x/csv_hacks.py
+++ python_project_2x/csv_hacks.py
@@ -27,6 +27,12 @@ def compile_csv(folder_path, dest_path, output_filename, first_col=""):
     # Create a list to hold all data frames
     df_list = []
 
+    # If only one file
+    only_one_file = False
+    
+    if len(csv_files) == 1:
+        only_one_file = True
+
     # Loop through each csv file
     for csv in csv_files:
         
@@ -45,7 +51,7 @@ def compile_csv(folder_path, dest_path, output_filename, first_col=""):
                 i += 1
 
             # Read the CSV file
-            df = pd.read_csv(csv_file, skiprows=i)
+            df = pd.read_csv(csv_file, skiprows=i-1)
 
             # Tell users how many rows being skipped and how many rows and columns being read
             print(f"Skipping {i} rows")
@@ -65,7 +71,7 @@ def compile_csv(folder_path, dest_path, output_filename, first_col=""):
                     i += 1
 
                 # Read the CSV file
-                df = pd.read_csv(csv_file, sep="\t", encoding="utf-16", skiprows=i)
+                df = pd.read_csv(csv_file, sep="\t", encoding="utf-16", skiprows=i-1)
 
                 # Tell users how many rows being skipped and how many rows and columns being read
                 print(f"Skipping {i} rows")
@@ -80,22 +86,46 @@ def compile_csv(folder_path, dest_path, output_filename, first_col=""):
         except Exception as e:
             print(f"Could not read file {csv} because of error: {e}")
     
-    # Concatenate all data frames in df_list
-    df_compile = pd.concat(df_list, ignore_index=True)
+    # Only one file or Concatenate all data frames in df_list
+    if only_one_file:
+        df_final = df
+    else:
+        df_final = pd.concat(df_list, ignore_index=True)
 
     # Get output_filename without extension (User can either give the filename with or without extension) 
     output_filename = output_filename.split(".")[0] + ".csv"
 
     # Export as CSV
-    df_compile.to_csv(os.path.join(dest_path, date_code_execute + "-" + output_filename), index=False)
-    print(f"Successfully compiled. Total no of rows = {df_compile.shape[0]}")
+    df_final.to_csv(os.path.join(dest_path, date_code_execute + "-" + output_filename), index=False)
+    print(f"Successfully compiled. Total no of rows = {df_final.shape[0]}")
 
-    return df_compile
+    return df_final
 
-# Test Code
-if __name__ == "__main__":
+# Test Col to Skip
+def test_col_to_skip():
+    folder_path = r"C:\Users\WeiZhenLim\OneDrive - 2X LLC\Work\Python\python_project_2x\01 Test\01 compile_csv Test\Col to Skip"
+    dest_path = r"C:\Users\WeiZhenLim\OneDrive - 2X LLC\Work\Python\python_project_2x\01 Test\01 compile_csv Test"
+    output_filename = r"Testing - Col to Skip"
+    first_col = r"Start Date (in UTC)"
+    compile_csv(folder_path, dest_path, output_filename, first_col)
+
+# Test Compile without Skipping
+def test_no_skip():
     folder_path = r"C:\Users\WeiZhenLim\OneDrive - 2X LLC\Work\Python\python_project_2x\01 Test\01 compile_csv Test\Compile Without Skipping"
     dest_path = r"C:\Users\WeiZhenLim\OneDrive - 2X LLC\Work\Python\python_project_2x\01 Test\01 compile_csv Test"
     output_filename = r"Testing - No Skip"
-    first_col = "Start Date (in UTC)"
     compile_csv(folder_path, dest_path, output_filename)
+
+# Test One File To Skip Row
+def  test_one_file():
+    folder_path = r"C:\Users\WeiZhenLim\OneDrive - 2X LLC\Work\Python\python_project_2x\01 Test\01 compile_csv Test\Only one File"
+    dest_path = r"C:\Users\WeiZhenLim\OneDrive - 2X LLC\Work\Python\python_project_2x\01 Test\01 compile_csv Test"
+    output_filename = r"Testing - Only One File"
+    first_col = r"*Account ID"
+    compile_csv(folder_path, dest_path, output_filename, first_col)
+
+# Test Code
+if __name__ == "__main__":
+    test_col_to_skip()
+    test_no_skip()
+    test_one_file()
