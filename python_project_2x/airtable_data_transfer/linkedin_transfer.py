c026ec5627f22c822af39d7610efb770991fc8c6
Wei Zhen Lim <WeiZhen.Lim@2x.marketing>
2024-03-04 14:40:44 +0800

    Complete until merging (Left compiling into Airtable format)

--- python_project_2x/airtable_data_transfer/linkedin_transfer.py
+++ python_project_2x/airtable_data_transfer/linkedin_transfer.py
@@ -90,7 +90,7 @@ def check_export_count(data, col_list=['Campaign Group ID', 'Campaign ID', 'Ad I
     `col_list` = list of columns to check. Default list of columns are "Campaign Group ID", "Campaign ID", and "Ad ID". \n
     """
     # Aggregate the campaign groups, campaign, and ads
-    result = data[col_list].apply(lambda col: col.unique())
+    result = data[col_list].apply(lambda col: col.nunique())
 
     display(result)
 
@@ -160,10 +160,10 @@ def preprocess_ads_performance_report(data):
     This function is used to preprocess ads performance report data and return the preprocessed data. 
     The preprocessing steps are:
     1. Clean column names (To remove symbols).
-    2. Removing duplicates.
-    3. Extract website and utm parameters from url (And Remove Ori URL Column).
-    4. Staging remap based on campaign objectives.
-    5. Subset relevant columns.
+    2. Extract website and utm parameters from url (And Remove Ori URL Column).
+    3. Staging remap based on campaign objectives.
+    4. Subset relevant columns.
+    5. Drop duplicates.
     ----------------------------------------------------------------
     Parameters: \n
     `data` = pandas data frame \n
@@ -173,9 +173,6 @@ def preprocess_ads_performance_report(data):
 
     # Clean column names (To remove symbols)
     data = _clean_column_names(data)
-
-    # Drop duplicates
-    data.drop_duplicates(inplace=True)
 
     # Extract website and utm parameters from url (And Remove Ori URL Column)
     data['Website URL'] = data['Click URL'].apply(lambda x: _url_to_website_utm(x, "website"))
@@ -193,10 +190,13 @@ def preprocess_ads_performance_report(data):
     req_col = ["Account Name", 
                "Campaign Group ID", "Campaign Group Name", "Campaign Group Status", 
                "Campaign ID", "Campaign Name", "Campaign Objective", "Campaign Type", "Campaign Status", 
-               "Cost Type", "Daily Budget", "Total Budget", 
-               "Creative Name", "Ad ID", "Creative Status", "Ad Introduction Text", "Ad Headline", "Ad Line", 
-               "Click URL", "Sponsored Update Type", "DSC Name", "Video Length (in Seconds)"]
+               "Cost Type",
+               "Creative Name", "Ad ID",
+               "Sponsored Update Type", "DSC Name", "Video Length (in Seconds)"]
     data = data[req_col]
+
+    # Drop duplicates
+    data.drop_duplicates(inplace=True)
 
     return data
 
@@ -207,6 +207,7 @@ def preprocess_ads_bulk_report(data):
     The preprocessing steps are:
     1. Clean column names (To remove symbols).
     2. Subset relevant columns.
+    3. Rename the "Creative ID" column to "Ad ID".
     ----------------------------------------------------------------
     Parameters: \n
     `data` = pandas data frame \n
@@ -218,12 +219,63 @@ def preprocess_ads_bulk_report(data):
     data = _clean_column_names(data)
 
     # Subset relevant columns
-    req_col = ["Account ID", 
+    req_col = ["Account ID",
                "Creative ID", "Creative Status", "Ad format", "Ad name", 
                "Introductory", "Headline", "Description", "Call to action", "Destination URL"]
     data = data[req_col]
 
+    # Rename the "Creative ID" column to "Ad ID"
+    data.rename(columns={"Creative ID": "Ad ID"}, inplace=True)
+
     return data
+
+# "Ad name" and "Creative Name" columns validation
+def _validate_ad_name(row):
+    """
+    This function is used to validate the "Ad name" column from `df_report` and "Creative Name" column from `df_bulk` based
+    on the following criteria:
+    1. If "Ad name" == "Creative Name", return "Ad name".
+    2. If "Ad name" == "", return "Creative Name".
+    3. If "Creative Name" == "", return "Ad name".
+    4. If both "Ad name" and "Creative Name" == "", return "".
+    """
+    if row['Ad name'] == row['Creative Name']:
+        return row['Ad name']
+    elif row['Ad name'] == "":
+        return row['Creative Name']
+    elif row['Creative Name'] == "":
+        return row['Ad name']
+    else:
+        return ""
+    
+
+# Merge and preprocess the merge data
+def merge_and_preprocess(df_report, df_bulk):
+    """
+    This function is used to merge and preprocess the merge data.
+    The preprocessing steps are:
+    1. Merge the two data frames.
+    2. Validate the "Ad name" column in `df_report` and "Creative Name" column in df_bulk.
+    3. Drop "Ad name" and "Creative Name" columns.
+    ----------------------------------------------------------------
+    Parameters: \n
+    `df_report` = processed ads performance report pandas data frame \n
+    `df_bulk` = processed ads bulk report pandas data frame \n
+    """
+    # Create a copy of the input data frame
+    df_report = df_report.copy()
+    df_bulk = df_bulk.copy()
+
+    # Merge two data frames
+    df_final = pd.merge(df_report, df_bulk, how='left', on="Ad ID") # Using left merge because bulk export can only export single image and text ads only
+
+    # Validate the "Ad name" and "Creative Name" columns
+    df_final['Ad Name'] = df_final.apply(_validate_ad_name, axis=1)
+
+    # Drop "Ad name" and "Creative Name" columns
+    df_final.drop(columns=["Ad name", "Creative Name"], inplace=True)
+
+    return df_final
 
 # Test Code
 if __name__ == "__main__":
@@ -241,6 +293,8 @@ if __name__ == "__main__":
     folder_path_bulk = r"C:\Users\WeiZhenLim\OneDrive - 2X LLC\Work\Python\python_project_2x\airtable_data_transfer\Ads Bulk Export Example"
     dest_folder = r"C:\Users\WeiZhenLim\OneDrive - 2X LLC\Work\Python\python_project_2x\airtable_data_transfer"
 
+    print("------------------------------------------------------------------------------------")
+
     # Read Ad Performance Report (Compile if necessary)
     df_report = compile_csv(folder_path_report, dest_folder, "ad_performance_report", "Start Date (in UTC)")
 
@@ -256,6 +310,5 @@ if __name__ == "__main__":
     # Preprocess LinkedIn Ads Bulk Report 
     df_bulk = preprocess_ads_bulk_report(df_bulk)
 
-    # Check the number of campaign groups, campaign, and ads
-    check_export_count(df_report)
-    check_export_count(df_bulk, ['Campaign Group ID', 'Campaign ID', 'Creative ID'])
+    print("------------------------------------------------------------------------------------")
+
