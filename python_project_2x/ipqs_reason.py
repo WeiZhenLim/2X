5cdd36eb3c91e5959bdb8dd0c0cb9136ba104032
Wei Zhen Lim <WeiZhen.Lim@2x.marketing>
2024-03-06 14:39:28 +0800

    Update ipqs_reason with Test

--- python_project_2x/ipqs_reason.py
+++ python_project_2x/ipqs_reason.py
@@ -3,6 +3,7 @@ import re
 import os
 from IPython.display import display 
 
+# This function label the email as invalid based on certain criteria from the IPQS results
 def _invalid_criteria(row):
     """
     This function label the email as invalid if any one of the following criteria is met:
@@ -24,6 +25,7 @@ def _invalid_criteria(row):
 
     return "Invalid" if is_invalid else "Valid"
 
+# This function compiles all the invalid reasons and display in a new column
 def _invalid_reason(row):
     """
     This function compiles all the invalid reasons in a list and display in a new column.
@@ -46,11 +48,12 @@ def _invalid_reason(row):
         if i:
             reasons.append(v)
     
-    return reasons
+    return "; ".join(reasons)
 
+# This function takes in IPQS results (CSV). Then checks for email validity and compiles the invalid reasons and no of invalid reasons.
 def ipqs_check_reason(filepath):
     """
-    This function takes the IPQS results (CSV) and check for the email validation and compile the invalid reason.
+    This function takes in IPQS results (CSV). Then checks for email validity and compiles the invalid reasons and no of invalid reasons.
     """
 
     df = pd.read_csv(filepath)
@@ -70,7 +73,7 @@ def ipqs_check_reason(filepath):
 
     # compile invalid reasons 
     df['Invalid_Reasons'] = df.apply(_invalid_reason, axis=1)
-    df['No_of_Invalid_Reasons'] = df['Invalid_Reasons'].apply(len)
+    df['No_of_Invalid_Reasons'] = df['Invalid_Reasons'].apply(lambda x: len(x.split(";")) if x != "" else 0)
 
     # extract file directory and filename
     file_dir = os.path.dirname(filepath)
@@ -83,3 +86,9 @@ def ipqs_check_reason(filepath):
     print(f"Output ({filename}) is save at {file_dir}")
 
     df.to_csv(full_filename, index=False)
+
+# NOTE: Test
+if __name__ == '__main__':
+    filepath = r"C:\Users\WeiZhenLim\OneDrive - 2X LLC\Work\Python\python_project_2x\01 Test\04 ipqs reason\LS-Backlog Batch 1-Finance-IPQS Result.csv"
+
+    ipqs_check_reason(filepath)
