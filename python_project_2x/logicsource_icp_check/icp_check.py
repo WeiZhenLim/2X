c8218afe20b066b9f2a77a5f87a438c6fefc5de4
Wei Zhen Lim <WeiZhen.Lim@2x.marketing>
2024-03-12 13:39:19 +0800

    Update contact's column name > "Company Domain" to "Company website"

--- python_project_2x/logicsource_icp_check/icp_check.py
+++ python_project_2x/logicsource_icp_check/icp_check.py
@@ -365,8 +365,8 @@ def zi_icp_check(data, filename="", is_company=True):
     
     contact_dict_rename = {"Job Role (Standardized)": "Job Role", "Job Function": "Job function", "Email Address": "Email", "Direct Phone Number": "Phone Number", 
                            "LinkedIn Contact Profile URL": "LinkedIn", "Person Street": "Street Address", "Person City": "City", "Person State": "State/Region", 
-                           "Person Zip Code": "Postal Code", "Country": "Country/Region", "Website": "Website URL", "Primary Industry": "Industry", 
-                           "Membership Note": "Membership Notes", "Source (Self-Define)": "Source Type"}
+                           "Person Zip Code": "Postal Code", "Country": "Country/Region", "Website": "Website URL", "Company Domain": "Company website",
+                           "Primary Industry": "Industry", "Membership Note": "Membership Notes", "Source (Self-Define)": "Source Type"}
 
 
     if not is_company:
